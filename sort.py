4b6f7312530c732b90fa71250758ac956308d1c8
lahiru <lahiru@vetstoria.com>
2018-08-05 11:57:47 +0530

    function improvement

--- sort.py
+++ sort.py
@@ -1,11 +1,29 @@
-def sortthis(list1):
+def sortUsingFor(list1, is_acending = True):
     for i in range(len(list1)):
             for k in range(i+1,len(list1)):
-                if list1[i]>list1[k]:
-                    abc = list1[i]
-                    list1[i],list1[k]=list1[k],abc
+                if(is_acending):
+                    if list1[i]>list1[k]:
+                        temp = list1[i]
+                        list1[i],list1[k]=list1[k],temp
+                else:
+                    if list1[i]<list1[k]:
+                        temp = list1[i]
+                        list1[i],list1[k]=list1[k],temp
     return list1
 
+    
+def sortUsingWhile(list1):
+    i = 0
+    while(i < len(list1)):
+        k = i+1
+        while(k < len(list1)):
+            if list1[i]>list1[k]:
+                    temp = list1[i]
+                    list1[i],list1[k]=list1[k],temp
+            k +=1
+        i += 1
+    return list1
 
 list_sample  =  [ 26, 25, 14, 55, 1, 2, 5 ]
-print(sortthis(list_sample))
+print(sortUsingFor(list_sample, False))
+print(sortUsingWhile(list_sample))
