68d3fb070f797e027e96fb201f53145ac4146a06
lahirusamaraweera <lahiru.studioxeon@gmail.com>
2018-09-30 10:49:35 +0530

    2015 AL

--- AL2015.py
+++ AL2015.py
@@ -1,10 +1,24 @@
 
-x = input('Enter Index number : ')
-m1 = input('Enter Index marks for subject 1 :')
-m2 = input('Enter Index marks for subject 1 :')
-m3 = input('Enter Index marks for subject 1 :')
+# compute
+def compute(fo):
+    index_no = 0
+    while(True):
+        index_no = int(input("Enter the index number : "))
+        if(-1 == index_no):
+            break
+        m1 = input('Enter Index marks for subject 1 :')
+        m2 = input('Enter Index marks for subject 2 :')
+        m3 = input('Enter Index marks for subject 3 :')
+        fo.write(compileIndex(index_no, m1, m2, m3))
 
+#complile the required string phase
+def compileIndex(index_no, m1, m2, m3):
+    return "index_no_"+str(index_no)+", mark_"+str(index_no)+str(m1)+", mark_"+str(index_no)+str(m2)+", mark_"+str(index_no)+str(m3)+"\n"
+    
+
+#real execution
+#open the file
 fo = open("marks.txt", "a")
-out = str(x)+" "+str(m1)+" "+str(m2)+" "+str(m3)
-fo.write(str(out))
+#call the compute function with the file object
+compute(fo)
 fo.close()
