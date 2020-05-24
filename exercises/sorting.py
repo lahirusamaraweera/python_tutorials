5bc691ea48b701928641a4c194a46174f188a6f2
lahiru <lahiru.studixeon@gmail.com>
2020-05-24 19:37:46 +0530

    weip

--- exercises/sorting.py
+++ exercises/sorting.py
@@ -11,6 +11,20 @@ def sortTheList(value_list):
     
     return value_list
 
+# Hand tracing the algorithm
+#   i   j   val..[i]    val..[j]    LIST
+#----------------------------------------------
+#   0   0   35          35          [35, 11, 54, 10 ]*
+#   0   1   35          11          [11, 35, 54, 10 ]
+#   0   2   11          54          [11, 35, 54, 10 ]
+#   0   3   11          10          [10, 35, 54, 11 ]
+#   1   1   35          35          [10, 35, 54, 11 ]*
+#   1   2   35          54          [10, 35, 54, 11 ]
+#   1   3   35          11          [10, 11, 54, 35 ]
+#   2   2   54          54          [10, 11, 54, 35 ]*
+#   2   3   54          35          [10, 11, 35, 54 ]
+#   3   3   54          54          [10, 11, 35, 54 ]*
+
 value_list = getInputFile()
 sorted_list = sortTheList(value_list)
 print(value_list)
